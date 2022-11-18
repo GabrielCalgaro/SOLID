98675a56905f539a0c1b23cd6265da6594323711
PC <PC@DESKTOP-QTI0624>
2022-11-18 08:20:04 -0400

    Adicionar arquivos de projeto.

---
+++ src/Alura.LeilaoOnline.WebApp/Controllers/LeilaoController.cs
@@ -0,0 +1,121 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Alura.LeilaoOnline.WebApp.Dados;
+using Alura.LeilaoOnline.WebApp.Models;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Controllers
+{
+    public class LeilaoController : Controller
+    {
+
+        AppDbContext _context;
+        LeilaoDao _leilaoDao;
+
+        public LeilaoController()
+        {
+            _context = new AppDbContext();
+            _leilaoDao = new LeilaoDao();
+        }        
+       
+        public IActionResult Index()
+        {
+            var leiloes = _leilaoDao.BuscarLeiloes();
+            return View(leiloes);
+        } 
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            ViewData["Categorias"] = _leilaoDao.BuscarCategorias();
+            ViewData["Operacao"] = "Inclusão";
+            return View("Form");
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Leilao model)
+        {
+            if (ModelState.IsValid)
+            {
+                _leilaoDao.Incluir(model);
+                return RedirectToAction("Index");
+            }
+            ViewData["Categorias"] = _leilaoDao.BuscarCategorias();
+            ViewData["Operacao"] = "Inclusão";
+            return View("Form", model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewData["Categorias"] = _leilaoDao.BuscarCategorias();
+            ViewData["Operacao"] = "Edição";
+            var leilao = _context.Leiloes.Find(id);
+            if (leilao == null) return NotFound();
+            return View("Form", leilao);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Leilao model)
+        {
+            if (ModelState.IsValid)
+            {
+                _leilaoDao.Alterar(model);
+                return RedirectToAction("Index");
+            }
+            ViewData["Categorias"] = _leilaoDao.BuscarCategorias();
+            ViewData["Operacao"] = "Edição";
+            return View("Form", model);
+        }
+
+        [HttpPost]
+        public IActionResult Inicia(int id)
+        {
+            var leilao = _leilaoDao.BuscarPorId(id);
+            if (leilao == null) return NotFound();
+            if (leilao.Situacao != SituacaoLeilao.Rascunho) return StatusCode(405);
+            leilao.Situacao = SituacaoLeilao.Pregao;
+            leilao.Inicio = DateTime.Now;
+            _leilaoDao.Alterar(leilao);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Finaliza(int id)
+        {
+            var leilao = _leilaoDao.BuscarPorId(id);
+            if (leilao == null) return NotFound();
+            if (leilao.Situacao != SituacaoLeilao.Pregao) return StatusCode(405);
+            leilao.Situacao = SituacaoLeilao.Finalizado;
+            leilao.Termino = DateTime.Now;
+            _leilaoDao.Alterar(leilao);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int id)
+        {
+            var leilao = _leilaoDao.BuscarPorId(id);
+            if (leilao == null) return NotFound();
+            if (leilao.Situacao == SituacaoLeilao.Pregao) return StatusCode(405);
+            _leilaoDao.Excluir(leilao);
+            return NoContent();
+        }
+
+        [HttpGet]
+        public IActionResult Pesquisa(string termo)
+        {
+            ViewData["termo"] = termo;
+            var leiloes = _leilaoDao.BuscarLeiloes()
+                .Where(l => string.IsNullOrWhiteSpace(termo) || 
+                    l.Titulo.ToUpper().Contains(termo.ToUpper()) || 
+                    l.Descricao.ToUpper().Contains(termo.ToUpper()) ||
+                    l.Categoria.Descricao.ToUpper().Contains(termo.ToUpper())
+                );
+            return View("Index", leiloes);
+        }
+    }
+}
